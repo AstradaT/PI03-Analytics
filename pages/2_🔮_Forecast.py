5e893da357a3e10fe449880997929145dce48f2c
AstradaT <tomi_cuervocasla@hotmail.com>
2022-09-28 19:03:32 -0300

    Add page for price predictions

---
+++ pages/2_🔮_Forecast.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import streamlit as st
+import requests
+from prophet import Prophet
+from prophet.plot import plot_plotly, plot_components_plotly
+from modules import get_historical_data
+
+
+# Markets elegidos
+coins = ['BTC', 'ETH', 'BNB', 'XRP', 'SOL',
+    'DOGE', 'DOT', 'MATIC', 'TRX', 'AVAX']
+
+coin = st.sidebar.selectbox(
+    'Choose a Coin', coins)
+
+st.title(f"Predict Future Price - {coin}")
+
+df = get_historical_data(coin)
+df = df[['date', 'close']]
+
+df.columns = ["ds", "y"]
+m = Prophet()
+m.fit(df);
+future = m.make_future_dataframe(periods=365)
+forecast = m.predict(future)
+
+st.plotly_chart(plot_plotly(m, forecast))
