45538a6a3b51af5b5d9dc38d2a37a1825f9b49e4
AstradaT <tomi_cuervocasla@hotmail.com>
2022-09-29 21:21:30 -0300

    Add button

--- pages/2_🔮_Forecast.py
+++ pages/2_🔮_Forecast.py
@@ -9,23 +9,22 @@ from modules import get_historical_data
 coins = ['BTC', 'ETH', 'BNB', 'XRP', 'SOL',
     'DOGE', 'MATIC', 'TRX', 'LTC', 'UNI']
 
-coin = st.sidebar.selectbox(
-    'Coin', coins)
+coin = st.sidebar.selectbox('Coin', coins)
+days = st.sidebar.number_input("Days to predict", min_value=1, max_value=365, value=7)
 
 col1, col2 = st.columns([1,8])
 col1.image(f"img/{coin.lower()}.png", width=64)
 col2.title(f"Predict Future Price - {coin}")
 
-df = get_historical_data(coin)
-df = df[['date', 'close']]
+if st.sidebar.button('Predict'):
+    df = get_historical_data(coin)
+    df = df[['date', 'close']]
+    df.columns = ["ds", "y"]
 
-df.columns = ["ds", "y"]
-m = Prophet()
-m.fit(df);
+    m = Prophet()
+    m.fit(df);
 
-days = st.sidebar.number_input("Days to predict", min_value=1, max_value=365, value=7)
+    future = m.make_future_dataframe(periods=days)
+    forecast = m.predict(future)
 
-future = m.make_future_dataframe(periods=days)
-forecast = m.predict(future)
-
-st.plotly_chart(plot_plotly(m, forecast, ylabel='Price (USD)', xlabel='Days'))
+    st.plotly_chart(plot_plotly(m, forecast, ylabel='Price (USD)', xlabel='Days'))
