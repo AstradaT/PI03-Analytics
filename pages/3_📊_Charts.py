c47005a20db3977448ca4f99a27da9ebea5af735
AstradaT <tomi_cuervocasla@hotmail.com>
2022-09-29 21:21:54 -0300

    Add buttons

--- pages/3_📊_Charts.py
+++ pages/3_📊_Charts.py
@@ -9,14 +9,16 @@ st.subheader("Compare volumes of transactions")
 coins = ['BTC', 'ETH', 'BNB', 'XRP', 'SOL',
     'DOGE', 'MATIC', 'TRX', 'LTC', 'UNI']
 
-coins_volumes = st.multiselect("Select coins to plot", coins, default=coins[0:4])
+coins_volumes = st.multiselect("Select coins to plot", coins, default=coins[3:6], key='volumes_sel')
 
-with st.spinner('Drawing chart...'):
-    st.pyplot(draw_volumes(coins_volumes))
+if st.button('Plot', key='volumes_btn'):
+    with st.spinner('Drawing chart...'):
+        st.plotly_chart(draw_volumes(coins_volumes), use_container_width=True)
 
 st.subheader("Compare historical prices")
 
-coins_prices = st.multiselect("Select coins to plot", coins, default=coins[0:2])
+coins_prices = st.multiselect("Select coins to plot", coins, default=coins[2::2], key='historical_prices_sel')
 
-with st.spinner('Drawing chart...'):
-    st.plotly_chart(draw_price_histories(coins_prices), use_container_width = True)
+if st.button('Plot', key='historical_prices_btn'):
+    with st.spinner('Drawing chart...'):
+        st.plotly_chart(draw_price_histories(coins_prices), use_container_width=True)
